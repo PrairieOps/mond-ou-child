adece91b76980c5b729557e7acc2601cc133b74d
Jason Sherman <jsn.sherman@gmail.com>
2018-07-30 17:12:37 -0500

    fix relative links in footer. Apply css from live test site.

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ function mond_ou_child_footer() {
     <div class="row-fluid">
         <div class="footerColumn span4">
             <div class="footerlogo"><img alt="OU" src="<?php echo get_stylesheet_directory_uri() ;?>/footerlogo.png"></div>
-            <div class="footeraddress"><a href="/architecture">College of Architecture</a><br>Gould Hall<br>
+            <div class="footeraddress"><a href="/">College of Architecture</a><br>Gould Hall<br>
 830 Van Vleet Oval<br>
 Norman, OK 73019<br>
 405.325.2444<br></div>       
@@ -59,14 +59,14 @@ Norman, OK 73019<br>
 
 
 <div class="text"><ul class="2columns unstyled"><div class="first column" style="width:50%; float: left;">
-<li><a href="/publicaffairs/WebPolicies/accessstatement">Accessibility</a></li>
-<li><a href="/sustainability">Sustainability</a></li>
+<li><a href="http://www.ou.edu//publicaffairs/WebPolicies/accessstatement">Accessibility</a></li>
+<li><a href="http://www.ou.edu//sustainability">Sustainability</a></li>
 <li><a href="http://ouhsc.edu/hipaa/">HIPAA</a></li>
 <li><a href="http://hr.ou.edu/jobs/">OU Job Search</a></li></div><div class="last column" style="width:50%; float: left;">
-<li><a href="/web/landing/policy">Policies</a></li>
-<li><a href="/web/landing/legalnotices">Legal Notices</a></li>
-<li><a href="/publicaffairs/WebPolicies/copyright">Copyright</a></li>
-<li><a href="/web/resources_offices">Resources &amp; Offices</a></li>
+<li><a href="http://www.ou.edu//web/landing/policy">Policies</a></li>
+<li><a href="http://www.ou.edu//web/landing/legalnotices">Legal Notices</a></li>
+<li><a href="http://www.ou.edu//publicaffairs/WebPolicies/copyright">Copyright</a></li>
+<li><a href="http://www.ou.edu//web/resources_offices">Resources &amp; Offices</a></li>
 </div><br style="clear:both;"></ul>
 </div>
 
@@ -95,7 +95,7 @@ Norman, OK 73019<br>
 <li><a target="_blank" href="https://www.facebook.com/OUCollegeofArch" class="facebook" adhocenable="false">facebook</a></li>
 <li><a target="_blank" href="https://twitter.com/OUCollegeofArch" class="twitter" adhocenable="false">twitter</a></li>
 <li><a target="_blank" href="http://www.linkedin.com/groups/OU-College-Architecture-Alumni-Friends-4123860?gid=4123860&amp;trk=hb_side_g" class="linkedin" adhocenable="false">linkedin</a></li>
-<li><a href="/web/socialmediadirectory" class="more">more</a></li>
+<li><a href="http://www.ou.edu/web/socialmediadirectory" class="more">more</a></li>
 </ul>
 </div>
 </div>
